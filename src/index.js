ce53a2f503265c0c1ef3f27f0f43868496e8382b
busranurkoroglu <busranur@publitas.com>
2026-01-02 20:46:59 +0100

    Improved the user experience

--- src/index.js
+++ src/index.js
@@ -16,9 +16,13 @@ function generatePoem(event) {
   let apiKey = "8ob48e0d163fa039b10t18b602a5fad5";
 
   let context =
-    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem in basic HTML and separate each line with a <br /> . Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
+    "You are a romantic Poem expert and love to write short poems. Your mission is to generate a 4 line poem and separate each line with a <br /> . Make sure to follow the user instructions. Do not include a title to the poem. Sign the poem with 'SheCodes AI' inside a <strong> element at the end of the poem and NOT at the beginning";
   let prompt = `User instructions: Generate a poem about ${instructionsInput.value}`;
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class = "generating"> ⏳ Generating a poem about ${instructionsInput.value}...</div>`;
 
   console.log("Generating poem");
   console.log("Prompt: ${prompt}");
